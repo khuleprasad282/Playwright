21478130ca2efc27897ce1d34616697ece86618a
khuleprasad282 <khuleprasad282@gmail.com>
2026-04-26 22:48:07 +0530

    Chapter 08: Add Function Basics and Type1 Function concepts

---
+++ Chapter_02_Java_Concepts/Chapter_04_Operators/chapter_05_Statements/chapter_06_Switch_Statements/chapter_07_Loops/Chapter_08_Arrays/76_Functions.js
@@ -0,0 +1,15 @@
+let score1 = 85;
+let result1 = score1 >= 70 ? "pass" : "Fail";
+console.log(result1);
+
+let score2 = 55;
+let result2 = score2 > 70 ? "pass" : "Fail";
+console.log(result2);
+
+function getResult(score) {
+    return score2 > =70 ? "Pass" : "Fail";
+}
+
+getResult(85);
+getResult(45);
+
