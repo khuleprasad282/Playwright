c3993e8c32e7d1c1e38b3512b9dbf78450f42064
khuleprasad282 <khuleprasad282@gmail.com>
2026-04-26 23:16:19 +0530

    Chapter 08: Add different types of function implementations (with/without args and returns)

---
+++ Chapter_02_Java_Concepts/Chapter_04_Operators/chapter_05_Statements/chapter_06_Switch_Statements/chapter_07_Loops/Chapter_08_Arrays/78_Type2_Fn_With_Arg_No_Return.js
@@ -0,0 +1,19 @@
+function greetByName(name) {
+    console.log("Hi", name);
+}
+
+greetByName("Prasad");
+greetByName("Rushikesh");
+greetByName("Mithelesh");
+greetByName("Prasad");
+
+function begger(money) {
+    console.log("Thanks", money);
+}
+
+let returnMesomething = begger(100);
+console.log(returnMesomething);
+
+let name1 = greetByName("Sumit");
+console.log(name1);
+
---
+++ Chapter_02_Java_Concepts/Chapter_04_Operators/chapter_05_Statements/chapter_06_Switch_Statements/chapter_07_Loops/Chapter_08_Arrays/79_Type2_Fn_Without_Arg_Return_Type.js
@@ -0,0 +1,7 @@
+function sayHello() {
+    console.log('hi');
+    return "Hello";
+}
+
+let relative = sayHello();
+console.log(relative);
