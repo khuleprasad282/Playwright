75cf055d6353c656164b7fd61c37f8875f10d5e3
khuleprasad282 <khuleprasad282@gmail.com>
2026-06-14 22:24:57 +0530

    Add 102_String.js containing string concept examples

---
+++ Chapter_02_Java_Concepts/Chapter_04_Operators/chapter_05_Statements/chapter_06_Switch_Statements/chapter_07_Loops/Chapter_08_Arrays/Chapter_09_Functions.js/chapter_10_String/102_String.js
@@ -0,0 +1,21 @@
+let url = "https://app.vwo.com";
+let status = "Pass";
+let message = "Test Completed in ${320}ms";
+
+let a = "hello";
+let b = "World";
+
+let name1 = "Prasad";
+let msg = "Hello,${name1}! 2 + 2 = ${2+2}";
+console.log(msg);
+
+let report = '
+Test: Login
+status: isPassing
+Duration: 320ms
+';
+
+console.log(String(200));
+String(true);
+String(null);
+String([1, 2]);
