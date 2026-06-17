c99cd6118878f061c2c4b2f6d7931df56a301891
khuleprasad282 <khuleprasad282@gmail.com>
2026-06-17 23:22:56 +0530

    Add 107_String_Conversion.js containing string conversion and immutability examples

---
+++ Chapter_02_Java_Concepts/Chapter_04_Operators/chapter_05_Statements/chapter_06_Switch_Statements/chapter_07_Loops/Chapter_08_Arrays/Chapter_09_Functions.js/chapter_10_String/107_String_Conversion.js
@@ -0,0 +1,18 @@
+(200).toString();
+true.toString();
+
+
+Number("42");
+
+parseInt("42px");
+parseFloat("3.14rem");
+
+
+let str = "hello";
+str[0] = "H";
+console.log(str);
+console.log(str);
+
+let upper = str.toUpperCase();
+console.log(str);
+console.log(upper); 
