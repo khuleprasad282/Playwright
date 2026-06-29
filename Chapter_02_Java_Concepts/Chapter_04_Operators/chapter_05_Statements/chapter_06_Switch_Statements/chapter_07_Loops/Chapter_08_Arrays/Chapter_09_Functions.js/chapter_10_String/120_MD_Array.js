72bac957e6fb63a826a9d64d5a9bc23012df8526
khuleprasad282 <khuleprasad282@gmail.com>
2026-06-29 23:34:28 +0530

    Add 120_MD_Array.js covering multi-dimensional arrays

---
+++ Chapter_02_Java_Concepts/Chapter_04_Operators/chapter_05_Statements/chapter_06_Switch_Statements/chapter_07_Loops/Chapter_08_Arrays/Chapter_09_Functions.js/chapter_10_String/120_MD_Array.js
@@ -0,0 +1,24 @@
+
+let results = ["pass", "fail", "pass"];
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+let matrix_2D = [
+    [1, 2, 3, 4],
+];
+console.log(" ---- ")
+let grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        console.log(grid[i][j]);
+    }
+}
